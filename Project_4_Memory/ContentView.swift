523e703b2bb1e7d9b193c8c2fe6e735e49f054ae
muskanmm <muskan.mankikar@gmail.com>
2024-03-22 00:20:31 -0400

    Initial Commit

---
+++ Project_4_Memory/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Project_4_Memory
+//
+//  Created by Muskan Mankikar on 3/22/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Project_4_Memory/Project_4_MemoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_4_MemoryApp.swift
+//  Project_4_Memory
+//
+//  Created by Muskan Mankikar on 3/22/24.
+//
+
+import SwiftUI
+
+@main
+struct Project_4_MemoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
